427651882f5fff2e49896a2192a8428abbee2817
Oyewole Oni <onioyewole1910@gmail.com>
2023-03-18 16:19:21 +0100

    Add css and animations

--- frontend/src/app/property/property-card/property-card.component.ts
+++ frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,14 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
-  Property: any ={
-    "Id": 1,
-    "Name": "Birla House",
-    "Type": "House",
-    "Price":1200,
-
-  }
-
+  @Input() property : any;
   constructor() { }
 
   ngOnInit() {
--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  Properties: Array<any> =[
+    {
+    "Id": 1,
+    "Name": "Birla House",
+    "Type": "House",
+    "Price":1200,
+
+  },
+  {
+    "Id": 2,
+    "Name": "Erose Villa",
+    "Type": "House",
+    "Price":1300,
+
+  },
+  {
+    "Id": 3,
+    "Name": "Oni white House",
+    "Type": "House",
+    "Price":1800,
+
+  },
+  {
+    "Id": 4,
+    "Name": "Oye Villa",
+    "Type": "House",
+    "Price":2200,
+
+  },
+  {
+    "Id": 5,
+    "Name": "Pearl Villa",
+    "Type": "House",
+    "Price":2400,
+
+  },
+]
+
   constructor() { }
 
   ngOnInit(): void {
